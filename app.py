1b8021026a576748390184a79988a676ce779821
Chad Smith <chadsmithmusic@icloud.com>
2021-11-01 19:18:07 -0700

    added logout route

--- app.py
+++ app.py
@@ -113,7 +113,9 @@ def login():
 def logout():
     """Handle logout of user."""
 
-    # IMPLEMENT THIS
+    do_logout()
+    flash("You have sucessfully logged out.", 'success')
+    return redirect('/login')
 
 
 ##############################################################################
